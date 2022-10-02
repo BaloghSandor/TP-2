6a063dd2fef64a9b4660b87b36f50a2e0eb2f5d4
baloghs0 <113210115+baloghs0@users.noreply.github.com>
2022-10-02 19:44:58 -0400

    Commit de l'exercice b) du TP#2

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 """
 Programme fait par Sandor Balogh
 Groupe: 403
-Description : Ce code est un jeu de devinette où le joueur doit deviner un nombre entre 1 et 100 et faire cela
-jusqu'au temps qu'il soit capable de le deviner. À la fin, on lui demande s'il voudrait continuer ou arrêter.
-Dans le cas où il veut continuer le programme recommence, mais dans le cas où il veut arrêter la boucle s'arrête.
-On dit également le nombre d'essaie qu'il falait pour que le joueur puisse deviner le nombre.
+Description : Ce code est un jeu de devinette où le joueur doit deviner un nombre entre deux nombres de son choix
+et faire cela jusqu'au temps qu'il soit capable de le deviner. À la fin, on lui demande s'il voudrait continuer
+ou arrêter. Dans le cas où il veut continuer le programme recommence, mais dans le cas où il veut arrêter la
+boucle s'arrête. On dit également le nombre d'essaie qu'il falait pour que le joueur puisse deviner le nombre.
 """
-
 
 from random import randint
 
 boucle_jeu = True
 
 while boucle_jeu:
-    valeur_cherche = randint(0, 100)
+    valeur_minimal = int(input('Choisisez la valeur minimale a deviner : '))
+    valeur_maximal = int(input('\nChoisisez la valeur maximale a deviner : '))
+    valeur_cherche = randint(valeur_minimal, valeur_maximal)
+
+    print('\nVous devez deviner une valeur entre', valeur_minimal, 'et', valeur_maximal)
 
     valeur_non_trouver = True
     nombre_de_essaies = 0
-
     while valeur_non_trouver:
         nombre_de_essaies += 1
-        essaie = int(input('Devinez le nombre choisi entre 1 et 100 : '))
+        essaie = int(input('\nEntrez votre essaie : '))
 
         if essaie == valeur_cherche:
             print('\nBravo! Vous avez trouvez la valeur en', nombre_de_essaies, 'essaies!')
-            recommencer = str(input('\nVoulez-vous recommencer (oui ou non)?'))
+            recommencer = str(input('\nVoulez-vous recommencer (oui ou non)? '))
 
             if recommencer == 'oui':
                 valeur_non_trouver = False
 
             elif recommencer == 'non':
-                print('Merci et aurevoir!')
+                print('\nMerci et aurevoir!')
                 valeur_non_trouver = False
                 boucle_jeu = False
 
