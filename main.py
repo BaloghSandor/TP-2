a49f4d2e6cbf0896e64aa95ce1bcd3e8712d4b5e
BaloghSandor <114366334+BaloghSandor@users.noreply.github.com>
2022-10-14 08:23:48 -0400

    Update main.py
    Version finale du code + commentaires

--- main.py
+++ main.py
@@ -11,18 +11,31 @@ from random import randint
 
 boucle_jeu = True
 
+
+def recommencer():
+    """
+    Cette fonction est invoquée au moment oû le joueur a pas réussi leur essaie, et on le demande s'il veut
+    quitter ou joueur une nouvelle partie.
+    """
+    global valeur_non_trouver
+    global boucle_jeu
+    global nombre_de_essaies
+    print('\nBravo! Vous avez trouvez la valeur en', nombre_de_essaies, 'essaies!')
+    demande_du_joueur = str(input('\nVoulez-vous recommencer (oui ou non)? '))
+
+    if demande_du_joueur == 'oui':
+        valeur_non_trouver = False
+
+    elif demande_du_joueur == 'non':
+        print('\nMerci et aurevoir!')
+        valeur_non_trouver = False
+        boucle_jeu = False
+
+
 while boucle_jeu:
 
     borne_minimale = int(input('Donner la valeur minimale à deviner : '))
     borne_maximale = int(input('Donner la valeur maximale à deviner : '))
-
-    def score():
-        """
-        Cette fonction est invoquée au moment oû le joueur n'a pas réussi leur essaie, afin de compter le nombre
-        de fois que le joueur a essayer, lui montrant son score à la fin du jeu.
-        """
-        global nombre_de_essaies
-        nombre_de_essaies = nombre_de_essaies + 1
 
     valeur_cherche = randint(borne_minimale, borne_maximale)
 
@@ -32,20 +45,11 @@ while boucle_jeu:
     nombre_de_essaies = 0
 
     while valeur_non_trouver:
-        score()
+        nombre_de_essaies = nombre_de_essaies + 1
         essaie = int(input('\nEntrez votre essaie : '))
 
         if essaie == valeur_cherche:
-            print(f'\nBravo! Vous avez trouvez la valeur en {nombre_de_essaies} essaies!')
-            recommencer = str(input('\nVoulez-vous recommencer (oui ou non)? '))
-
-            if recommencer == 'oui':
-                valeur_non_trouver = False
-
-            elif recommencer == 'non':
-                print('\nMerci et aurevoir!')
-                valeur_non_trouver = False
-                boucle_jeu = False
+            recommencer()
 
         elif essaie > valeur_cherche:
             print('\nOups! Le nombre que vous cherchez est plus petit que', essaie)
